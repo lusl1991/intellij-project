0dd59284fb0dc309ae473bde4c52f459e8694c75
lusl1991 <825729444@qq.com>
2017-09-26 20:20:01 +0800

    map排序测试

--- webportal-model/src/main/java/com/softel/model/utils/KeyComparator.java
+++ webportal-model/src/main/java/com/softel/model/utils/KeyComparator.java
@@ -5,21 +5,24 @@ import java.util.*;
 
 public class KeyComparator {
 
-    public static void main(String[] args){
-        List<Map.Entry<Integer,String>> mappingList = null;
-        Map<Integer,String> map = new TreeMap<Integer,String>();
-        map.put(5, "month");
-        map.put(2, "bread");
-        map.put(8, "attack");
-
-        //通过ArrayList构造函数把map.entrySet()转换成list
-        mappingList = new ArrayList<Map.Entry<Integer,String>>(map.entrySet());
+    public static List<Map.Entry<Integer,String>> sort(Map<Integer, String> map){
+        List<Map.Entry<Integer,String>> mappingList = new ArrayList<Map.Entry<Integer,String>>(map.entrySet());
         //通过比较器实现比较排序
         Collections.sort(mappingList, new Comparator<Map.Entry<Integer,String>>(){
             public int compare(Map.Entry<Integer,String> mapping1,Map.Entry<Integer,String> mapping2){
                 return mapping1.getKey().compareTo(mapping2.getKey());
             }
         });
+        return mappingList;
+    }
+
+    public static void main(String[] args){
+        Map<Integer,String> map = new TreeMap<Integer,String>();
+        map.put(5, "month");
+        map.put(2, "bread");
+        map.put(8, "attack");
+
+        List<Map.Entry<Integer,String>> mappingList = sort(map);
 
         for(Map.Entry<Integer,String> mapping:mappingList){
             System.out.println(mapping.getKey()+":"+mapping.getValue());
