bf3ac08fc2d635755aa4145e6bffd0ef994a4f76
lusl1991 <lusl1991@163.com>
2018-05-11 12:14:33 +0800

    更新数据库脚本文件

--- webportal-web/src/main/resources/sql/db.sql
+++ webportal-web/src/main/resources/sql/db.sql
@@ -39,23 +39,23 @@ CREATE TABLE `tb_menu` (
   `ISLEAF` tinyint(1) NOT NULL COMMENT '是否子节点（0：是；1：否）',
   `PARENTID` int(12) NOT NULL DEFAULT '0' COMMENT '父级菜单id（0表示当前菜单为一级菜单）',
   `ORDERID` int(12) NOT NULL COMMENT '排序',
+  `ZT` tinyint(1) NOT NULL COMMENT '状态（0：可用；1：不可用）',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_menu
 -- ----------------------------
-INSERT INTO `tb_menu` VALUES ('1', 'anticon-shop', 'menu.form', '', '1', '1', '0', '1');
-INSERT INTO `tb_menu` VALUES ('2', '', 'menu.info', '/shop/info', '2', '0', '1', '2');
-INSERT INTO `tb_menu` VALUES ('3', '', 'menu.type', '/shop/type', '2', '0', '1', '3');
-INSERT INTO `tb_menu` VALUES ('4', '', 'menu.table', '/shop/merchant', '2', '0', '1', '4');
-INSERT INTO `tb_menu` VALUES ('5', '', 'menu.layout', '/shop/layout', '2', '0', '1', '5');
-INSERT INTO `tb_menu` VALUES ('6', '', 'menu.sync', '/shop/sync', '2', '0', '1', '6');
-INSERT INTO `tb_menu` VALUES ('7', '', 'menu.async', '/shop/async', '2', '0', '1', '7');
-INSERT INTO `tb_menu` VALUES ('8', 'anticon-shopping-cart', 'menu.chart', '', '1', '1', '0', '8');
-INSERT INTO `tb_menu` VALUES ('9', 'anticon-shopping-cart', 'menu.order', '/order/profile', '2', '0', '8', '9');
-INSERT INTO `tb_menu` VALUES ('10', 'anticon-appstore', '商品', '', '1', '1', '0', '10');
-INSERT INTO `tb_menu` VALUES ('11', 'anticon-appstore', '添加商品', '', '2', '0', '10', '11');
-INSERT INTO `tb_menu` VALUES ('12', 'anticon-appstore', '商品分类', '', '2', '1', '10', '12');
-INSERT INTO `tb_menu` VALUES ('13', 'anticon-appstore', '品牌设置', '', '3', '0', '12', '13');
-INSERT INTO `tb_menu` VALUES ('14', 'anticon-appstore', '规格分类', '', '3', '0', '12', '14');
+INSERT INTO `tb_menu` VALUES ('1', 'anticon-shop', 'menu.form', '', '1', '1', '0', '1', '0');
+INSERT INTO `tb_menu` VALUES ('2', '', 'menu.info', '/shop/info', '2', '0', '1', '2', '0');
+INSERT INTO `tb_menu` VALUES ('3', '', 'menu.type', '/shop/type', '2', '0', '1', '3', '0');
+INSERT INTO `tb_menu` VALUES ('4', '', 'menu.layout', '/shop/layout', '2', '0', '1', '5', '0');
+INSERT INTO `tb_menu` VALUES ('5', '', 'menu.sync', '/shop/sync', '2', '0', '1', '6', '0');
+INSERT INTO `tb_menu` VALUES ('6', '', 'menu.async', '/shop/async', '2', '0', '1', '7', '0');
+INSERT INTO `tb_menu` VALUES ('7', 'anticon-shopping-cart', 'menu.chart', '', '1', '1', '0', '8', '0');
+INSERT INTO `tb_menu` VALUES ('8', 'anticon-shopping-cart', 'menu.order', '/order/profile', '2', '0', '8', '9', '0');
+INSERT INTO `tb_menu` VALUES ('9', 'anticon-appstore', 'menu.goods', '', '1', '1', '0', '10', '0');
+INSERT INTO `tb_menu` VALUES ('10', 'anticon-appstore', 'menu.goods.property', '', '2', '0', '10', '11', '0');
+INSERT INTO `tb_menu` VALUES ('11', 'anticon-appstore', 'menu.goods.classify', '', '2', '1', '10', '12', '0');
+INSERT INTO `tb_menu` VALUES ('12', 'anticon-appstore', 'menu.goods.brand', '', '3', '0', '12', '13', '0');
+INSERT INTO `tb_menu` VALUES ('13', 'anticon-appstore', 'menu.goods.size', '', '3', '0', '12', '14', '0');
